2c03da6b22f26851927e47dbfa2c0c5fd53b6e57
Pedro-Moreno-Ban <pemorenouar@gmail.com>
2025-06-08 18:18:43 -0500

    Update vehiculos.js

--- routes/vehiculos.js
+++ routes/vehiculos.js
@@ -5,5 +5,5 @@ const vehiculoController = require('../controllers/vehiculos');
 // Asegúrate de pasar la función directamente
 router.get('/', vehiculoController.getVehiculos);
 
-// Exporta SOLO el router
+// Exporta SOLO el router (no un objeto)
 module.exports = router;
