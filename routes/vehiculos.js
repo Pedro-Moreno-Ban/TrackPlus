f9c18f63efa836f61fbbb2d842a8133ea5c7a806
Pedro-Moreno-Ban <pemorenouar@gmail.com>
2025-06-08 18:01:00 -0500

    vehiculos.js

--- routes/vehiculos.js
+++ routes/vehiculos.js
@@ -1,7 +1,10 @@
+// routes/vehiculos.js
 const express = require('express');
 const router = express.Router();
 const { getVehiculos } = require('../controllers/vehiculos');
 
+// Asegúrate de que getVehiculos sea una función
 router.get('/', getVehiculos);
 
-module.exports = router; 
+// Exporta SOLO el router (no un objeto)
+module.exports = router;
