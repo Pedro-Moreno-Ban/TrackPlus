9f78a61e5c8e28e4358d6fb845afc9bfaf4816be
Pedro-Moreno-Ban <pemorenouar@gmail.com>
2025-06-08 17:56:56 -0500

    app.js

--- app.js
+++ app.js
@@ -6,23 +6,24 @@ dotenv.config();
 
 const app = express();
 
-// Middlewares esenciales
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
-// Rutas
-const vehiculosRouter = require('./routes/vehiculos'); 
+// Importación de rutas
+const vehiculosRouter = require('./routes/vehiculos');
 const alertasRouter = require('./routes/alertas');
 
+// Uso de rutas
 app.use('/api/vehiculos', vehiculosRouter);
 app.use('/api/alertas', alertasRouter);
 
-// Ruta de prueba opcional
+// Ruta básica de prueba
 app.get('/', (req, res) => {
   res.send('API Funcionando');
 });
 
-// Middleware de errores (¡ANTES de module.exports!)
+// Middleware de errores (debe estar ANTES de module.exports)
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ 
@@ -32,4 +33,3 @@ app.use((err, req, res, next) => {
 });
 
 module.exports = app;
-});
